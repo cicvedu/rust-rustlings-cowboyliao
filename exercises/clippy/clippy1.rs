6121764caf60aaefeee30b19a313595a542ae3b2
cowboyliao <2730647052@qq.com>
2023-08-09 14:54:25 +0800

    test

--- exercises/clippy/clippy1.rs
+++ exercises/clippy/clippy1.rs
@@ -9,9 +9,11 @@
 // Execute `rustlings hint clippy1` or use the `hint` watch subcommand for a
 // hint.
 
+
 use std::f32;
 
 fn main() {
+    // let pi = 3.14f32;
     let pi = f32::consts::PI;
     let radius = 5.00f32;
 
